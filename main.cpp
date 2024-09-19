9f74796961dfdd10b31e44ef4d0b22bd97d4b167
Enakaev Aleksey <enakaev.aleksey@yandex.ru>
2024-09-19 12:52:57 +0300

    main: Удален весь лишний код

--- main.cpp
+++ main.cpp
@@ -8,60 +8,10 @@
 #include <QDebug>
 
 
-namespace alexen {
-namespace nmh {
-namespace io {
-
-
-QByteArray read( FILE* istream  = stdin )
-{
-     QFile inputFile;
-     qDebug() << inputFile.open( istream, QIODevice::ReadOnly );
-
-     std::int32_t nmhLen = {};
-     if( inputFile.read( reinterpret_cast< char* >( &nmhLen ), sizeof( nmhLen ) ) != sizeof( nmhLen )
-          || nmhLen < 0 )
-     {
-          throw std::runtime_error{ "data length read failed" };
-     }
-     if( inputFile.atEnd() )
-     {
-          throw std::runtime_error{ "eof reached" };
-     }
-     const auto data = inputFile.read( nmhLen );
-     if( data.size() != nmhLen )
-     {
-          throw std::runtime_error{ "data body read failed" };
-     }
-     qDebug() << data;
-     return data;
-}
-
-
-void write( const QByteArray& data, FILE* ostream = stdout )
-{
-     QFile outputFile;
-     qDebug() << outputFile.open( ostream, QIODevice::WriteOnly );
-     const std::int32_t nmhLen = data.size();
-     qDebug() << "Write len:" << outputFile.write( reinterpret_cast< const char* >( &nmhLen ), sizeof( nmhLen ) );
-     qDebug() << "Write data:" << outputFile.write( data );
-}
-
-
-} // namespace io
-} // namespace nmh
-} // namespace alexen
-
-
-
 int main( int argc, char *argv[] )
 {
      try
      {
-          alexen::nmh::io::write( alexen::nmh::io::read() );
-
-          return 0;
-
           QApplication app{ argc, argv };
 
           alexen::nmh::Application application;
